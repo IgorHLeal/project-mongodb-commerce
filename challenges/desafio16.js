f7b1110f61122abf8048940064de1fccbb6c27c4
Igor Leal <igorhenriqueleal3@gmail.com>
2022-10-17 16:57:14 -0300

    feat: Finalizando desafio 19

--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -1,4 +1,4 @@
-db.produtos.updateOne (
+db.produtos.updateOne(
   { nome: { $eq: "Big Mac" } },
   { $currentDate: { ultimaModificacao: true } },
 );
--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,5 +1,5 @@
 db.resumoProdutos.insertOne(
-  { franquia: "McDonalds", totalProdutos:  5 },
+  { franquia: "McDonalds", totalProdutos: 5 },
 );
 
 db.resumoProdutos.find(
