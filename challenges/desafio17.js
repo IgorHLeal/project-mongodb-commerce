68f930314ddbf8c9b43d178ea9bcb4e123da68ad
Igor Leal <igorhenriqueleal3@gmail.com>
2022-10-17 17:18:27 -0300

    feat: Finalizando desafio 22

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -3,8 +3,8 @@ const totalProdutos = db.produtos.count();
 db.resumoProdutos.insertOne(
   {
     franquia: "McDonalds",
-    totalProdutos
-  }
+    totalProdutos,
+  },
 );
 
 db.resumoProdutos.find(
