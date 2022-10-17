0942f30884e543e3af52a6a7d7e682a4e0b54066
Igor Leal <igorhenriqueleal3@gmail.com>
2022-10-17 17:02:34 -0300

    refactor: Implementando nova forma de resolução do requisito 17

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,5 +1,10 @@
+const totalProdutos = db.produtos.count();
+
 db.resumoProdutos.insertOne(
-  { franquia: "McDonalds", totalProdutos: 5 },
+  {
+    franquia: "McDonalds",
+    totalProdutos
+  }
 );
 
 db.resumoProdutos.find(
@@ -10,3 +15,7 @@ db.resumoProdutos.find(
     _id: 0,
   },
 );
+
+/* Refatorando requisito:
+Adicionando uma constante;
+Resolução encontrada em uma thread de Airel Ribeiro: https://trybecourse.slack.com/archives/C02TH6V3MC5/p1666034996028089?thread_ts=1666034775.825169&cid=C02TH6V3MC5 */
